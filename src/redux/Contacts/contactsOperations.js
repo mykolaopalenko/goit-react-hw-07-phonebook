1bf1c5a59724b618298af0368bca9aef77f97f18
Mykola Opalenko <mykola.opalenko@gmail.com>
2022-12-07 11:53:17 +0200

    fix

--- src/redux/Contacts/contactsOperations.js
+++ src/redux/Contacts/contactsOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://6334a6fa849edb52d6f585a9.mockapi.io/api';
+axios.defaults.baseURL = 'https://6390615465ff418311119e22.mockapi.io/api/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
